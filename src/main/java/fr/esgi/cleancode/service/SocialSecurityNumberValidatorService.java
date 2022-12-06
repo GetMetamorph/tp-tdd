d5f92ce8f8b0e24313c4ec114d9979c2d7c78637
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-12-06 16:22:03 +0100

    [GREEN] Add code to SocialSecurityNumberValidatorService
    [GREEN] Add code to SocialSecurityNumberValidatorService that return True if is Valid, else return False

--- src/main/java/fr/esgi/cleancode/service/SocialSecurityNumberValidatorService.java
+++ src/main/java/fr/esgi/cleancode/service/SocialSecurityNumberValidatorService.java
@@ -3,6 +3,10 @@ package fr.esgi.cleancode.service;
 public class SocialSecurityNumberValidatorService {
 
         public boolean isValid(String socialSecurityNumber) {
-
+                return socialSecurityNumber != null
+                        && !socialSecurityNumber.isEmpty()
+                        && !socialSecurityNumber.isBlank()
+                        && socialSecurityNumber.matches("[0-9]+")
+                        && socialSecurityNumber.length() == 15;
         }
 }
